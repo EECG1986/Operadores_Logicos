616c8784da59c4aa57b4420deb7609d824c98b84
EECG1986 <estivencardona@gmail.com>
2024-02-18 18:10:16 -0500

    Operadores_Logicos.js

---
+++ Operadores_Logicos.js
@@ -0,0 +1,14 @@
+let condicionA = true;
+console.log(condicionA);
+
+let condicionB = false;
+console.log(condicionB);
+
+let ambasTrue = condicionA && condicionB;
+console.log(ambasTrue);
+
+let algunaTrue = condicionA || condicionB;
+console.log(algunaTrue);
+
+let condicionANegada = !condicionA;
+console.log(condicionANegada);
